495f73a4093b7c85054403094e530015c1d75fe3
Ayush Agrawal <ayushagrawalspr12345@gmail.com>
2025-08-14 14:40:25 +0530

    V2

--- shell.sh
+++ shell.sh
@@ -3,8 +3,12 @@
 # Update the system
 sudo apt update
 
-# Clone our File
-git clone https://github.com/Empty-Hacker/Netflix-Clone.git
-
 # Install Docker on the instance
 sudo apt install docker.io -y
+
+# Clone our File
+git clone https://github.com/AyushAgraw/Netflix-clone-deployment.git
+
+cd Netflix-clone-deployment
+
+sudo docker build -t netflix-clone .
